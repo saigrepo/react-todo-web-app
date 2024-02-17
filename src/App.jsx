0894809c1a0548c505c2d0e6af3ab51892b0faef
Saiganesh2023 <iasganesh123456@gmail.com>
2024-02-17 17:17:25 +0530

    Commit of version 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
 
+  const [tasks,setTasks] = useState([]);
+  const [newTask,setNewTask] = useState("");
+
+  function handleAddTodo () {
+      setTasks(t => [...t,newTask]);
+      setNewTask("");
+  }
+
+  function handleInputChange(event) {
+    setNewTask(event.target.value);
+  }
+
+  function handleDeleteTask(index) {
+      const updatedTasks = tasks.filter( (_,i) => i!=index);
+      setTasks(updatedTasks);
+  }
+
+  function handleTaskDone(event, index) {
+  const taskVal = "task"+index;
+    event.target.checked ? document.getElementById(taskVal).style.color = "red" :
+        document.getElementById(taskVal).style.color = "black"
+
+  }
+
   return (
-      <div>Welcome to React & Vite app, <span>Saiganesh</span></div>
+      <div>
+        <div className="todo-header">To-do App</div>
+        <div className="todo-add-task">
+          <input type="text" value={newTask} placeholder="Enter What is on your mind" onChange={handleInputChange}/>
+          <button className="todo-add-task-button" onClick={handleAddTodo}>Add Task</button>
+        </div>
+        <ul>
+          {tasks.map((task,i) =>
+            <li key={i}><input type="checkbox" onChange={(e) => handleTaskDone(e,i)}/><p id={"task"+i}>{task}</p> <button className="todo-delete-task-button" onClick={() => handleDeleteTask(i)}>Delete</button></li>
+          )}
+        </ul>
+      </div>
   )
 }
 
